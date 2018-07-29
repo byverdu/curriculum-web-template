73b9b128ffeec9d6111909337a18178a04553d50
byverdu <byverdu@gmail.com>
2018-07-29 20:04:19 +0100

    added tests for headContentConfig

--- __tests__/content.test.js
+++ __tests__/content.test.js
@@ -9,12 +9,26 @@ describe('Content data', () => {
     const {
       headContentConfig
     } = config;
+    const headProperties = [
+      {key: 'description', value: 'Albert Vallverdu Resume'},
+      {key: 'keywords', value: 'HTML5, SCSS, JavaScript, Node, Jest, ES6, Gulp'},
+      {key: 'author', value: '@byverdu'}
+    ];
   
     it( 'should be defined', () => {
       expect(headContentConfig).toBeDefined();
     });
     it( 'should be an object', () => {
-      expect(headContentConfig).toEqual({});
+      expect(headContentConfig).toBeInstanceOf(Object);
     });
+    it( 'should have 3 properties', () => {
+      expect(Object.keys(headContentConfig)).toHaveLength(3);
+    });
+    headProperties.forEach(prop => {
+      const { key, value } = prop;
+      it(`should have property ${key} with value ${value}`, () => {
+        expect(headContentConfig[key]).toContain(value)
+      });
+    })
   })
 });
--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,8 @@
-const headContentConfig = {};
+const headContentConfig = {
+  description: 'Albert Vallverdu Resume',
+  keywords: 'HTML5, SCSS, JavaScript, Node, Jest, ES6, Gulp',
+  author: '@byverdu'
+};
 
 export const config = {
   headContentConfig
